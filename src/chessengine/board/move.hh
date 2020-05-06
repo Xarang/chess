98d4254037706d41dc6a3fa5ab7cd712899d871a
Victor Coatalem <victor.coatalem@epita.fr>
2020-05-06 17:59:23 +0200

    [MISC] added comments to move

--- src/chessengine/board/move.hh
+++ src/chessengine/board/move.hh
@@ -6,18 +6,34 @@
 namespace board
 {
     struct Move {
+        //the type of the piece moved
         PieceType piece_;
+        //the position where the piece is moved
         Position end_position_;
-
+        //ths position from which the piece moves (used in case of ambiguities)
         std::optional<Position> start_position_;
+        //a pawn is getting promotted to a new piecetype
         std::optional<PieceType> promotion_;
 
+        //the move is a capture
         bool is_capture_;
+
+        //the move puts the opponent in check
         bool is_check_;
+
+        //the move puts the opponent in checkmate
         bool is_checkmate_;
+
+        //the move is the special case of "double pawn push"
         bool is_double_pawn_push_;
+
+        //the move is the special case of "king castling"
         bool is_king_castling_;
+
+        //the move is the special case of "queen castling"
         bool is_queen_castling_;
+
+        //the move is the special case of "en passant"
         bool is_en_passant_;
 
         Move(PgnMove pgnmove); //TODO: code this in move.cc
