405376f452d57a6ea8c5e2893c26f8ff47918227
Victor Coatalem <victor.coatalem@epita.fr>
2020-05-09 03:26:49 +0200

    [LISTENER][INIT] working on implementing our own listener

--- src/chessengine/board/color.hh
+++ src/chessengine/board/color.hh
@@ -9,5 +9,13 @@ namespace board
         BLACK = true
     };
 
-
+    std::string color_to_string(Color c) {
+        if (c == Color::WHITE) {
+            return "White";
+        }
+        if (c == Color::BLACK) {
+            return "Black";
+        }
+        return "Unknown color";
+    }
 } // namespace board
