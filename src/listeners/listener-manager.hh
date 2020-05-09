f8342cb8721cb3e2ca0114dca402c3ebbf477e86
Victor Coatalem <victor.coatalem@epita.fr>
2020-05-09 16:58:59 +0200

    [LISTENER][FIX] now registers the piece that got captured, not the one that captured

--- src/listeners/listener-manager.hh
+++ src/listeners/listener-manager.hh
@@ -29,7 +29,7 @@ namespace listener {
             void run_perft(int depth);
 
             //send informations about what happened on the chessboard to all listeners
-            void register_move(board::Color color, board::Move move, std::optional<board::Piece> destinationSquare);
+            void register_move(board::Color color, board::Move move, std::optional<board::Piece> captured_piece);
             void register_check(board::Color color);
             void register_mat(board::Color color);
             void register_pat(board::Color color);
