74facb4e04aabb087139bce73fc6415da4967e28
Xarang <victor.coatalem@epita.fr>
2020-05-09 18:37:43 +0200

    tried to do the FEN format (#8)
    Co-authored-by: Emilien Gaury <emilien.gaury@epita.fr>

--- src/chessengine/board/piece.hh
+++ src/chessengine/board/piece.hh
@@ -23,6 +23,45 @@ namespace board
         {}
 
         std::list<struct Move> getAllPotentialMoves();
+
+        inline char piece_to_char_fen() {
+            if (this->color_ == Color::WHITE) {
+                switch (this->type_) {
+                    case PieceType::PAWN:
+                        return 'P';
+                    case PieceType::KNIGHT:
+                        return 'N';
+                    case PieceType::BISHOP:
+                        return 'B';
+                    case PieceType::ROOK:
+                        return 'R';
+                    case PieceType::QUEEN:
+                        return 'Q';
+                    case PieceType::KING:
+                        return 'K';
+                    default:
+                        return ' ';
+                }
+            }
+            else {
+                switch (this->type_) {
+                    case PieceType::PAWN:
+                        return 'p';
+                    case PieceType::KNIGHT:
+                        return 'n';
+                    case PieceType::BISHOP:
+                        return 'b';
+                    case PieceType::ROOK:
+                        return 'r';
+                    case PieceType::QUEEN:
+                        return 'q';
+                    case PieceType::KING:
+                        return 'k';
+                    default:
+                        return ' ';
+                }
+            }
+        }
     };
     
 }
