a487696b9ba71d1d4812975b8f2771b0b0822996
nicolas.caron <nicolas.caron@epita.fr>
2020-05-15 16:51:28 +0200

    [SNAKE AND NO DEBUG]

--- cmake-build-debug/CMakeFiles/listener-test-suite.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for listener-test-suite.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/listener-test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/listener-test"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/listener-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
