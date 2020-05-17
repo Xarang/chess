3e8953578d46cd30eda359386f05a494382e5f3d
Victor Coatalem <victor.coatalem@epita.fr>
2020-05-17 18:25:12 +0200

    [TESTS][WIP] working on ai automated testing

---
+++ tests/tsuite-files/uci_match.py
@@ -0,0 +1,34 @@
+import chess
+import subprocess
+import sys
+import threading
+import queue
+
+
+chessengine="./chessengine"
+otherengine="./stockfish"
+
+def initialise_engine(path):
+    engine_process = subprocess.Popen([path], universal_newlines=True, bufsize=1, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    print("launched " + path)
+
+    engine_process.stdin.write("uci\n")
+    engine_process.stdin.write("isready\n")
+
+    print("sent uci introduction")
+
+
+    for stdout_line in iter(engine_process.stdout.readline, ""):
+        if (stdout_line.strip() == "readyok"):
+            break
+        print("got line from process: " + stdout_line)
+
+
+    return engine_process
+
+
+chessengine_process = initialise_engine("./" + sys.argv[1])
+
+chessengine_process.kill()
+
+#otherengine_process = subprocess.Popen([otherengine_process])
